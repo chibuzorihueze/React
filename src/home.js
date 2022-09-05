0edb89427076df9167560a9183a911b86a0f2c95
Chibuzor Ihueze <krisykord@gmail.com>
2022-09-05 17:41:01 +0100

    updates

---
+++ src/home.js
@@ -0,0 +1,16 @@
+const Home = () => {
+    const handleclik = () =>console.log("hello");
+    const handleclick = (name) =>{
+        console.log(`hello ${name}`);
+    }
+
+    return ( 
+        <div className="home">
+            <h2>Homepage</h2>
+            <button onClick={handleclik}>click me</button>
+            <button onClick={()=>{handleclick("tilewa")}}>click</button>
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ src/navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+        <h1>logo</h1>
+        <div>
+            <a href="" className="links"> home</a>
+            <a href=""> blog</a>
+        </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
